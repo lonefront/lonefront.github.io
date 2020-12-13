49c10497c172d385d2fb42850609fb5b6453b226
Ross Hutchens <rlhcxm@gmail.com>
2020-12-13 00:39:36 -0600

    Branch for page turning prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    red: '-100%',
+    ylw: '-100%',
+    blk: '-100%'
+  }
+
+  turn = e => {
+    // q targets div element name to 'turn page'
+    let q = e.target.id
+    switch (q) {
+      case 'blue':
+        this.setState({
+          red: '0%'
+        });
+        break;
+      case 'red':
+        this.setState({
+          ylw: '0%'
+        });
+        break;
+      case 'yellow':
+        this.setState({
+          blk: '0%'
+        });
+        break;
+      default:
+        this.setState({
+          red: '-100%',
+          ylw: '-100%',
+          blk: '-100%'      
+        })
+        break;
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div id="blue" className="screen" onClick={this.turn}></div>
+        <div id="red" className="screen" style={{bottom: this.state.red}} onClick={this.turn}></div>
+        <div id="yellow" className="screen" style={{bottom: this.state.ylw}} onClick={this.turn}></div>
+        <div id="black" className="screen" style={{bottom: this.state.blk}} onClick={this.turn}></div>
+      </>
+      );  
+  }
 }
 
 export default App;
