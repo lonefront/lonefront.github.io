11d39d41cda8ca4c5e55734d83a1015ea55d8766
Ross Hutchens <rlhcxm@gmail.com>
2020-12-19 23:54:10 -0600

    Set up positioning for header elements, however the app is not optimized for small displays

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import './Header.css'
 import logo from '../../assets/logotype.svg'
 import line from '../../assets/line.svg'
+import sigil from '../../assets/sigil.svg'
+// import grid from '../../assets/grid-png.png'
+import tab from '../../assets/tab.svg'
 
 class Header extends Component {
 
@@ -9,9 +12,29 @@ class Header extends Component {
     return(
       <div className="header">
         <img src={logo} alt="Lonefront" className="logotype"/>
-        <img src={line} alt="line"></img>
-        <div className="bar"></div>
-      </div>
+        <img src={sigil} alt="Logo" className="sigil"/>
+        {/* <img src={grid} alt="grid pattern" className="grid"/> */}
+        <img src={line} alt="line" className="line"/>
+        {/* <div className="bar"></div> */}
+          <main className="header-tags">
+            <div>
+              <img src={tab} alt="tab" className="tag"/>
+              SLIDETYPE:[]
+            </div>
+            <div>
+              <img src={tab} alt="tab" className="tag"/>
+              INDEX:[]
+            </div>
+            <div>
+              <img src={tab} alt="tab" className="tag"/>
+              TIMESTAMP:[]
+            </div>
+            <div>
+              <img src={tab} alt="tab" className="tag"/>
+              [INSERT PHRASE VARIABLE]
+            </div>
+          </main>
+        </div>
     )
   }
 }
